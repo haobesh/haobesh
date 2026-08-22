9b138b8f444131bfefafc1303d1083d699aeff83
haobesh <voratr.0@gmail.com>
2026-08-22 16:03:27 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,145 @@
+"use strict";
+
+const CACHE_NAME = "haobesh-v1";
+
+const FILES_TO_CACHE = [
+
+  "./",
+
+  "./index.html",
+
+  "./style.css",
+
+  "./script.js",
+
+  "./manifest.json"
+
+];
+
+self.addEventListener(
+
+  "install",
+
+  (event) => {
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+
+        .then((cache) => {
+
+          return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+        .then(() => {
+
+          return self.skipWaiting();
+
+        })
+
+    );
+
+  }
+
+);
+
+self.addEventListener(
+
+  "activate",
+
+  (event) => {
+
+    event.waitUntil(
+
+      caches.keys()
+
+        .then((cacheNames) => {
+
+          return Promise.all(
+
+            cacheNames
+
+              .filter(
+
+                (name) => name !== CACHE_NAME
+
+              )
+
+              .map(
+
+                (name) => caches.delete(name)
+
+              )
+
+          );
+
+        })
+
+        .then(() => {
+
+          return self.clients.claim();
+
+        })
+
+    );
+
+  }
+
+);
+
+self.addEventListener(
+
+  "fetch",
+
+  (event) => {
+
+    if (event.request.method !== "GET") {
+
+      return;
+
+    }
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then((response) => {
+
+          const copy =
+
+            response.clone();
+
+          caches.open(CACHE_NAME)
+
+            .then((cache) => {
+
+              cache.put(
+
+                event.request,
+
+                copy
+
+              );
+
+            });
+
+          return response;
+
+        })
+
+        .catch(() => {
+
+          return caches.match(
+
+            event.request
+
+          );
+
+        })
+
+    );
+
+  }
+
+);
